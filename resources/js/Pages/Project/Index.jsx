6129847cfa209bb3ba0b9899f4237035af7786c2
xose-aka <xusniddin-qodirov@mail.ru>
2024-08-14 22:10:07 +0200

    projects table

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -15,7 +15,39 @@ export default function Index({auth, projects}) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            {JSON.stringify(projects, undefined, 2)}
+                            <table className={"w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"}>
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400
+                                                  border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-2">Id</th>
+                                        <th className="px-3 py-2">Image</th>
+                                        <th className="px-3 py-2">Name</th>
+                                        <th className="px-3 py-2">Status</th>
+                                        <th className="px-3 py-2">Create Date</th>
+                                        <th className="px-3 py-2">Due Date</th>
+                                        <th className="px-3 py-2">Created by</th>
+                                        <th className="px-3 py-2">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { projects.data.map(project => (
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td className="px-3 py-2">{ project.id }</td>
+                                            <td className="px-3 py-2">
+                                                <img src={ project.image_path }/>
+                                            </td>
+                                            <td className="px-3 py-2">{ project.name }</td>
+                                            <td className="px-3 py-2">{ project.status }</td>
+                                            <td className="px-3 py-2">{ project.created_at }</td>
+                                            <td className="px-3 py-2">{ project.due_date }</td>
+                                            <td className="px-3 py-2">{ project.createdBy.name }</td>
+                                            <td className="px-3 py-2">
+
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
