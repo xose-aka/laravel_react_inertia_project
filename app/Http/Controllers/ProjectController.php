140c40ee5f400e6281d154613f5e1e31fc3fcd58
xose-aka <xusniddin-qodirov@mail.ru>
2024-08-28 23:00:45 +0200

    project's form image

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -41,7 +41,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return inertia("Project/Crate");
+        return inertia("Project/Create");
     }
 
     /**
--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -1,7 +1,24 @@
 import Authenticated from "@/Layouts/AuthenticatedLayout.jsx";
-import {Head, Link} from "@inertiajs/react";
+import {Head, Link, router, useForm} from "@inertiajs/react";
+import InputLabel from "@/Components/InputLabel.jsx";
+import TextInput from "@/Components/TextInput.jsx";
 
 export default function Create({auth}) {
+
+    const {data, setData, post, processing, errors, reset} = useForm({
+        image: '',
+        name: '',
+        status: '',
+        description: '',
+        due_date: '',
+    })
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        post(route('projects.store'))
+    }
+
     return (
         <Authenticated
             user={auth.user}
@@ -16,9 +33,19 @@ export default function Create({auth}) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            Form
-                        </div>
+                        <form onSubmit={onSubmit} className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div>
+                                <InputLabel htmlFor={'project_image_path'} value={"Project Image"}/>
+                                <TextInput
+                                    id={'project_image_path'}
+                                    type={'file'}
+                                    name={'image'}
+                                    value={ data.image }
+                                    className="mt-1 block w-full"
+                                    onChange={ e => setData('image', e.target.value) }
+                                />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
